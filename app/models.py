20df1a1182a66e78521d9dffc8181e5f12630d24
Igboke Daniel <danieligboke669@gmail.com>
2025-05-24 21:52:16 +0100

    Add Task model with status enumeration and relationship to User model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,16 @@
-from sqlalchemy import Column, String, Integer,Boolean, DateTime, ForeignKey
+from sqlalchemy import String, Integer,Boolean, DateTime, ForeignKey
 from datetime import datetime, timezone
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
+from sqlalchemy import Enum as SQLEnum
+import enum
+
+# Define an enumeration for task status
+class TaskStatus(enum.Enum):
+    PENDING = "pending"
+    IN_PROGRESS = "in progress"
+    COMPLETED = "completed"
+
 
 class User(Base):
     __tablename__ = "users"
@@ -11,5 +20,21 @@ class User(Base):
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean,default=True)
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
-    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False, onupdate=lambda: datetime.now(timezone.utc)
-    )
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False, onupdate=lambda: datetime.now(timezone.utc))
+    tasks: Mapped[list["Task"]] = relationship("Task", back_populates="user")
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=True)
+    status: Mapped[TaskStatus] = mapped_column(SQLEnum(TaskStatus), default=TaskStatus.PENDING, nullable=False)
+    due_date: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False, onupdate=lambda: datetime.now(timezone.utc))
+    # Define a foreign key relationship to the User model(foreign key column)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    # Define a relationship to the User model
+    # This allows you to access the user associated with a task
+    user: Mapped["User"] = relationship("User", back_populates="tasks")
+    
