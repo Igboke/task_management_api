e51d93fe11dd3a6148d8160832ecf8b7ae8c485f
Igboke Daniel <danieligboke669@gmail.com>
2025-05-25 18:49:10 +0100

    Add tasks router to API with /tasks prefix

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,9 +1,10 @@
 # app/api/v1/api.py
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import users
+from app.api.v1.endpoints import users, tasks
 
 api_router = APIRouter()
 
-# Include the books router under the /users prefix
+api_router.include_router(tasks.router,prefix="/tasks", tags=["Tasks"])
+# Include the users router under the /users prefix
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
